598c6624eff81940519522eee556261d8150e553
tal0020 <teekayl2001@gmail.com>
2022-02-01 18:09:10 -0500

    Second navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SecondTopComponent } from './header/second.top.component';
 import { TopComponent } from './header/top.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopComponent
+    TopComponent,
+    SecondTopComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/header/second.top.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'newegg-second-top',
+    templateUrl: 'second.top.component.html',
+    styleUrls: ['second.top.component.css']
+})
+export class SecondTopComponent {
+
+}
